68d53c21b5333c73c0039bea964a85f5703fef55
AhmetArtunBostan <ahmetartunbostan@gmail.com>
2025-02-20 15:15:55 +0300

    Fix offline mode and service worker

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'weather-app-v1';
+const CACHE_NAME = 'weather-app-v2';
 const STATIC_CACHE = [
-    '.',
-    'index.html',
-    'css/style.css',
-    'css/animations.css',
-    'js/weather.js',
-    'images/icons/icon-72x72.png',
-    'images/icons/icon-96x96.png',
-    'images/icons/icon-128x128.png',
-    'images/icons/icon-144x144.png',
-    'images/icons/icon-152x152.png',
-    'images/icons/icon-192x192.png',
-    'images/icons/icon-384x384.png',
-    'images/icons/icon-512x512.png',
-    'manifest.json',
-    'offline.html'
+    './',
+    './index.html',
+    './css/style.css',
+    './css/animations.css',
+    './js/weather.js',
+    './images/icons/icon-72x72.png',
+    './images/icons/icon-96x96.png',
+    './images/icons/icon-128x128.png',
+    './images/icons/icon-144x144.png',
+    './images/icons/icon-152x152.png',
+    './images/icons/icon-192x192.png',
+    './images/icons/icon-384x384.png',
+    './images/icons/icon-512x512.png',
+    './manifest.json',
+    './offline.html'
 ];
 
 // Install Service Worker
@@ -26,33 +26,39 @@ self.addEventListener('install', event => {
                 return cache.addAll(STATIC_CACHE);
             })
     );
+    self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+    return self.clients.claim();
 });
 
 // Fetch Event
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request)
                     .then(response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                        if (response) {
                             return response;
                         }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-                        return response;
-                    })
-                    .catch(() => {
-                        if (event.request.url.includes('/images/')) {
-                            return caches.match('images/icons/icon-72x72.png');
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('./offline.html');
                         }
-                        return caches.match('offline.html');
+                        if (event.request.url.includes('/images/')) {
+                            return caches.match('./images/icons/icon-72x72.png');
+                        }
                     });
             })
     );
